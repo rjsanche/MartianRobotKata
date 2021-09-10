6c3c6569f9a17ed49b87d85f41d25f3824611b67
Raúl Jiménez <rjsanche@gmail.com>
2021-09-10 15:01:10 +0200

    Add project files.

---
+++ MartianRobot/MartianRobotEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MartianRobot
+{
+    public class MartianRobotEngine
+    {
+        #region Fields
+        private string _orientation;
+        #endregion
+
+        #region Constructor
+        public MartianRobotEngine(string orientation)
+        {
+            _orientation = orientation;
+        }
+        #endregion
+
+        #region Public methods
+        public void TurnRight()
+        {
+            _orientation = "E";
+        }
+
+        public string GetOrientation()
+        {
+            return "E";
+        }
+        #endregion
+    }
+}
---
+++ MartianRobotTest/MartianRobotShould.cs
@@ -0,0 +1,22 @@
+using MartianRobot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MartianRobotTest
+{
+    [TestClass]
+    public class MartianRobotShould
+    {
+        [TestMethod]
+        public void WhenStartsOrientationIsNorthThenTurnRightWhenReceiveRightInstruction()
+        {
+            //arrange
+            MartianRobotEngine robot = new MartianRobotEngine("N");
+            var expected = "E";
+            //act
+            robot.TurnRight();
+            var result = robot.GetOrientation();
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
