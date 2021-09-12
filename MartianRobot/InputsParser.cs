70a4b5c44a69a42e70052028a7b8174ce802567b
Raúl Jiménez <rjsanche@gmail.com>
2021-09-12 17:00:41 +0200

    Refactor

--- MartianRobot/InputsParser.cs
+++ MartianRobot/InputsParser.cs
@@ -13,14 +13,30 @@ namespace MartianRobot
         {
             orientationTypes orientation = orientationTypes.Unknown;
             string[] inputs = input.Split(" ");
+            bool firstPosition = true;
+            int x_position = 0;
+            int y_position = 0;
             foreach (string s in inputs)
             {
                 if (Char.IsLetter(s[0]))
                 {
                     orientation = ParseOrientation(s[0].ToString());
                 }
+                else
+                {
+                    if (firstPosition)
+                    {
+                        x_position = int.Parse(s);
+                    }
+                    else
+                    {
+                        y_position = int.Parse(s);
+                    }
+                    firstPosition = !firstPosition;
+                }
+
             }
-            return new Position() { x = 1, y = 1, orientation = orientation };
+            return new Position() { x = x_position, y = y_position, orientation = orientation };
         }
 
         private static orientationTypes ParseOrientation(string orientation)
