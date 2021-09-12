54ba9dccd4fb2e8adfdb389573459f2f9af5d456
Raúl Jiménez <rjsanche@gmail.com>
2021-09-12 16:28:22 +0200

    Refactor

--- MartianRobot/MartianRobotEngine.cs
+++ MartianRobot/MartianRobotEngine.cs
@@ -1,29 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MartianRobot
 {
     public class MartianRobotEngine
     {
         #region Fields
-        private string _orientation;
+
+        public enum orientationTypes
+        {
+            N,
+            E,
+            S,
+            W,
+            Unknown
+        };
+
+        private orientationTypes _orientation;
+        private List<Tuple<string, orientationTypes>> _orientationParser = new List<Tuple<string, orientationTypes>>
+        {
+            new Tuple<string, orientationTypes>("N", orientationTypes.N),
+            new Tuple<string, orientationTypes>("E", orientationTypes.E),
+            new Tuple<string, orientationTypes>("S", orientationTypes.S),
+            new Tuple<string, orientationTypes>("W", orientationTypes.W)
+        };
+
         #endregion
 
         #region Constructor
         public MartianRobotEngine(string orientation)
         {
-            _orientation = orientation;
+            _orientation = ParseOrientation(orientation);
         }
         #endregion
 
         #region Public methods
         public void TurnRight()
         {
-            _orientation = "E";
+            _orientation = GetNext(_orientation);
         }
 
         public string GetOrientation()
         {
-            return "E";
+            return Enum.GetName(_orientation);
+        }
+        #endregion
+
+        #region Private
+
+        private orientationTypes ParseOrientation(string orientation)
+        {
+            orientationTypes result = orientationTypes.Unknown;
+            Enum.TryParse<orientationTypes>(orientation, true, out result);
+            return result;
+        }
+
+        private orientationTypes GetNext(orientationTypes currentOrientation)
+        {            
+            orientationTypes nextOrientation = currentOrientation + 1;            
+            if(nextOrientation == orientationTypes.Unknown)
+            {
+                nextOrientation = orientationTypes.N;
+            }
+            return nextOrientation;
         }
         #endregion
     }
--- MartianRobotTest/MartianRobotShould.cs
+++ MartianRobotTest/MartianRobotShould.cs
@@ -18,5 +18,8 @@ namespace MartianRobotTest
             //assert
             Assert.AreEqual(expected, result);
         }
+
+
+
     }
 }
